ce88b09a228c90b9322bf9864883ecf841b3349a
Ana-Rojas88 <anac.rojas88@gmail.com>
2022-10-24 09:53:27 -0500

    first commit

---
+++ src/routes/Router.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+
+
+
+const App = () => {
+
+    return (
+        <BrowserRouter>
+           <h1>Hola</h1>
+        </BrowserRouter>
+    )
+}
+
+export default App;
