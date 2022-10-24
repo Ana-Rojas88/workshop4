57e9acea1b4103edb38871f765f8888a9b8b3311
camilon0 <kmilo8433@gmail.com>
2022-10-24 10:11:50 -0500

    creacion rama develop

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,16 +1,12 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-
-
-
 const App = () => {
-
-    return (
-        <BrowserRouter>
-           <h1>Hola</h1>
-        </BrowserRouter>
-    )
-}
+  return (
+    <BrowserRouter>
+      <h1>Hola develop</h1>
+    </BrowserRouter>
+  );
+};
 
 export default App;
